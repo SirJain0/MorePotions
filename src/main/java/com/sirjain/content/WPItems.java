2a0f7cd22083a7edce5061e63e3941a95e79aba4
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-07-31 20:17:03 +0100

    Finish texture, add to group

--- src/main/java/com/sirjain/content/WPItems.java
+++ src/main/java/com/sirjain/content/WPItems.java
@@ -1,7 +1,9 @@
 package com.sirjain.content;
 
 import com.sirjain.WPMain;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -16,5 +18,7 @@ public class WPItems {
                         new Identifier(WPMain.MOD_ID, "withered_ashes"),
                         new Item(new Item.Settings())
                 );
+
+                ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register((entries -> entries.add(WITHERED_ASHES)));
         }
 }
