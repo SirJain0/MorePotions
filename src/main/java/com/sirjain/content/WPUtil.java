668754ef82bcb08f433df770152d10e501463bc5
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-08-14 11:44:37 -0400

    Add levitating potion and arrows

--- src/main/java/com/sirjain/content/WPUtil.java
+++ src/main/java/com/sirjain/content/WPUtil.java
@@ -14,9 +14,11 @@ import net.minecraft.util.registry.Registry;
 public class WPUtil {
         public static Potion WITHER_POTION, LONG_WITHER_POTION, STRONG_WITHER_POTION;
         public static Potion HUNGER_POTION, LONG_HUNGER_POTION, STRONG_HUNGER_POTION;
+        public static Potion LEVITATING_POTION, LONG_LEVITATING_POTION, STRONG_LEVITATING_POTION;
 
         public static StatusEffect WITHER = StatusEffects.WITHER;
         public static StatusEffect HUNGER = StatusEffects.HUNGER;
+        public static StatusEffect LEVITATION = StatusEffects.LEVITATION;
 
         // Registers potions
         public static void registerPotions() {
@@ -27,6 +29,10 @@ public class WPUtil {
                 HUNGER_POTION = registerPotion("hunger_potion", new Potion(new StatusEffectInstance(HUNGER, 20 * 30, 0)));
                 LONG_HUNGER_POTION = registerPotion("long_hunger_potion", new Potion(new StatusEffectInstance(HUNGER, 20 * 60, 0)));
                 STRONG_HUNGER_POTION = registerPotion("strong_hunger_potion", new Potion(new StatusEffectInstance(HUNGER, 20 * 22, 1)));
+
+                LEVITATING_POTION = registerPotion("levitating_potion", new Potion(new StatusEffectInstance(LEVITATION, 20 * 16, 0)));
+                LONG_LEVITATING_POTION = registerPotion("long_levitating_potion", new Potion(new StatusEffectInstance(LEVITATION, 20 * 28, 0)));
+                STRONG_LEVITATING_POTION = registerPotion("strong_levitating_potion", new Potion(new StatusEffectInstance(LEVITATION, 20 * 10, 1)));
         }
 
         // Registers potion recipes
@@ -38,6 +44,10 @@ public class WPUtil {
                 BrewingRecipeRegistryMixin.addRecipe(Potions.WATER, Items.ROTTEN_FLESH, HUNGER_POTION);
                 BrewingRecipeRegistryMixin.addRecipe(HUNGER_POTION, Items.REDSTONE, LONG_HUNGER_POTION);
                 BrewingRecipeRegistryMixin.addRecipe(LONG_HUNGER_POTION, Items.GLOWSTONE_DUST, STRONG_HUNGER_POTION);
+
+                BrewingRecipeRegistryMixin.addRecipe(Potions.LEAPING, Items.SHULKER_SHELL, LEVITATING_POTION);
+                BrewingRecipeRegistryMixin.addRecipe(LEVITATING_POTION, Items.REDSTONE, LONG_LEVITATING_POTION);
+                BrewingRecipeRegistryMixin.addRecipe(LONG_LEVITATING_POTION, Items.GLOWSTONE_DUST, STRONG_LEVITATING_POTION);
         }
 
         // Helper method to shorten process of registering potions
