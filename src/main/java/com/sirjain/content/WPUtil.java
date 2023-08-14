32c625199b3011c87f57da57ea265d8a03867596
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-08-13 21:22:32 -0400

    Add hunger potions and arrows

--- src/main/java/com/sirjain/content/WPUtil.java
+++ src/main/java/com/sirjain/content/WPUtil.java
@@ -14,13 +14,20 @@ import net.minecraft.util.Identifier;
 
 public class WPUtil {
         public static Potion WITHER_POTION, LONG_WITHER_POTION, STRONG_WITHER_POTION;
+        public static Potion HUNGER_POTION, LONG_HUNGER_POTION, STRONG_HUNGER_POTION;
+
         public static StatusEffect WITHER = StatusEffects.WITHER;
+        public static StatusEffect HUNGER = StatusEffects.HUNGER;
 
         // Registers potions
         public static void registerPotions() {
                 WITHER_POTION = registerPotion("wither_potion", new Potion(new StatusEffectInstance(WITHER, 20 * 16, 0)));
                 LONG_WITHER_POTION = registerPotion("long_wither_potion", new Potion(new StatusEffectInstance(WITHER, 20 * 36, 0)));
                 STRONG_WITHER_POTION = registerPotion("strong_wither_potion", new Potion(new StatusEffectInstance(WITHER, 20 * 10, 1)));
+
+                HUNGER_POTION = registerPotion("hunger_potion", new Potion(new StatusEffectInstance(HUNGER, 20 * 30, 0)));
+                LONG_HUNGER_POTION = registerPotion("long_hunger_potion", new Potion(new StatusEffectInstance(HUNGER, 20 * 60, 0)));
+                STRONG_HUNGER_POTION = registerPotion("strong_hunger_potion", new Potion(new StatusEffectInstance(HUNGER, 20 * 22, 1)));
         }
 
         // Registers potion recipes
@@ -28,6 +35,10 @@ public class WPUtil {
                 BrewingRecipeRegistryMixin.addRecipe(Potions.AWKWARD, WPItems.WITHERED_ASHES, WITHER_POTION);
                 BrewingRecipeRegistryMixin.addRecipe(WITHER_POTION, Items.REDSTONE, LONG_WITHER_POTION);
                 BrewingRecipeRegistryMixin.addRecipe(LONG_WITHER_POTION, Items.GLOWSTONE_DUST, STRONG_WITHER_POTION);
+
+                BrewingRecipeRegistryMixin.addRecipe(Potions.WATER, Items.ROTTEN_FLESH, HUNGER_POTION);
+                BrewingRecipeRegistryMixin.addRecipe(HUNGER_POTION, Items.REDSTONE, LONG_HUNGER_POTION);
+                BrewingRecipeRegistryMixin.addRecipe(LONG_HUNGER_POTION, Items.GLOWSTONE_DUST, STRONG_HUNGER_POTION);
         }
 
         // Helper method to shorten process of registering potions
