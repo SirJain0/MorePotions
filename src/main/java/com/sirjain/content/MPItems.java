b78e0e804d00d9b29468ba6bb96cfcdf7da4323e
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-08-14 12:09:56 -0400

    Rename mod

--- src/main/java/com/sirjain/content/MPItems.java
+++ src/main/java/com/sirjain/content/MPItems.java
@@ -1,19 +1,19 @@
 package com.sirjain.content;
 
-import com.sirjain.WPMain;
+import com.sirjain.MPMain;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
-public class WPItems {
+public class MPItems {
         public static Item WITHERED_ASHES;
 
         // Registers mod items
         public static void registerItems() {
                 WITHERED_ASHES = Registry.register(
                         Registry.ITEM,
-                        new Identifier(WPMain.MOD_ID, "withered_ashes"),
+                        new Identifier(MPMain.MOD_ID, "withered_ashes"),
                         new Item(new Item.Settings().group(ItemGroup.BREWING))
                 );
         }
